bc38388d6e8465ff12bca6291cffa7a9fcf27eb2
sakshi <sakshi.panchale@dorset.ie>
2025-09-30 14:49:53 +0100

    demos

---
+++ oop-20250930-a-switch-with-exceptions/Models/SwitchDemoDriver.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_20250930_a_switch_with_exceptions.Models
+{
+    internal class SwitchDemoDriver
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Switch Demo");
+            NestedIfDemo();
+            SequentialIfDemo();
+            SwitchDemo();
+
+
+        }
+
+        public static void NestedIfDemo()
+        {
+            Console.WriteLine("Nested If Demo");
+            int number = 3;
+            if (number == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("Three");
+            }
+            else
+            {
+                Console.WriteLine("Other");
+            }
+        }
+
+        public static void SequentialIfDemo()
+        {
+            Console.WriteLine("SequentialIfDemo");
+            int number = 3;
+            if (number == 1)
+            {
+                Console.WriteLine("One");
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            if (number == 3)
+            {
+                Console.WriteLine("Three");
+            }
+
+        }
+
+        public static void SwitchDemo()
+        {
+            Console.WriteLine("Switch Demo");
+            int number = 3;
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+
+        }
+    }
+}
--- oop-20250930-a-switch-with-exceptions/Program.cs
+++ oop-20250930-a-switch-with-exceptions/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using oop_20250930_a_switch_with_exceptions.Models;
+
+SwitchDemoDriver.Run();
